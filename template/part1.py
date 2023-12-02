c442e324bff5d7ad92cb4e19ef2b97aa8a263b9f
Tony McNulty <toneee@gmail.com>
2023-12-02 16:57:18 +0000

    Minor improvement to template script layout

--- template/part1.py
+++ template/part1.py
@@ -4,6 +4,8 @@ def get_input(filename):
     with open(filename, 'r') as file:
         return file.read().split()
 
-# Main
-lines = get_input('input.txt')
+def main():
+    lines = get_input('example.txt')
 
+if __name__ == "__main__":
+    main()
