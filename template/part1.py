37e703c90ede89f0506316d6423f7d14fd1b7efe
Tony McNulty <toneee@gmail.com>
2023-12-03 09:13:20 +0000

    Template improvements

--- template/part1.py
+++ template/part1.py
@@ -1,11 +1,25 @@
 import os
 
+DEBUG_MODE = True
+PUZZLE_INPUT = 'example.txt'
+
+# Day X: <puzzle name>, Part 1
+def main():
+    lines = get_input('example.txt')
+
+
+# Puzzle solution functions below
+
+
+# Generic helper functions below
 def get_input(filename):
     with open(filename, 'r') as file:
         return file.read().split()
 
-def main():
-    lines = get_input('example.txt')
+def debug(message):
+    if DEBUG_MODE:
+        print(message)
 
+# Call the main method only when running this script directly
 if __name__ == "__main__":
     main()
--- template/part2.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_input(filename):
-    with open(filename, 'r') as file:
-        return file.read().split()
-
-# Main
-lines = get_input('input.txt')
-
